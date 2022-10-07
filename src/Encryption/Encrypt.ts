ac5af7e448ff0bfe9af40af21cdf2972563d7687
Sai Krishna <saikingstyle@gmail.com>
2022-10-07 07:31:12 +0530

    added the enccryption code to be used for further purpose

---
+++ src/Encryption/Encrypt.ts
@@ -0,0 +1,25 @@
+// const crypto = require('crypto'); // you cannot require is it becaue it is thorowing an error saying that it is block scoped.
+import crypto from "crypto"; // and the entire module works and I have checked it so have fun.
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+function encrypt(text) {
+ let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+ let encrypted = cipher.update(text);
+ encrypted = Buffer.concat([encrypted, cipher.final()]);
+ return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+function decrypt(text) {
+ let iv = Buffer.from(text.iv, 'hex');
+ let encryptedText = Buffer.from(text.encryptedData, 'hex');
+ let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+ let decrypted = decipher.update(encryptedText);
+ decrypted = Buffer.concat([decrypted, decipher.final()]);
+ return decrypted.toString();
+}
+
+var hw = encrypt("Some serious stuff")
+console.log(hw)
+console.log(decrypt(hw))
